796ee398e44fcf1d7abf8d7be18317d674f30e73
imrushikesh77 <rushikeshjawale2004@gmail.com>
2024-06-25 14:51:15 +0530

    resolved cors issue

--- server/app.js
+++ server/app.js
@@ -1,8 +1,17 @@
 import express from 'express';
+import cors from 'cors';
+
 const app = express();
 
 import router from './routes/code.route.js';
 
+
+const corsOptions = {
+    origin: 'https://librm.vercel.app',
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
